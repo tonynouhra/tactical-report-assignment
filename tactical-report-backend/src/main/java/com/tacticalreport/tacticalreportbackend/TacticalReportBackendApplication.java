0c2d5814df9e76d74e45853ff28e3667df521240
tonynouhra <78659244+tonynouhra@users.noreply.github.com>
2025-11-09 23:32:44 +0200

    Set up initial Spring Boot project with Gradle

---
+++ tactical-report-backend/src/main/java/com/tacticalreport/tacticalreportbackend/TacticalReportBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tacticalreport.tacticalreportbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacticalReportBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacticalReportBackendApplication.class, args);
+    }
+
+}
---
+++ tactical-report-backend/src/test/java/com/tacticalreport/tacticalreportbackend/TacticalReportBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tacticalreport.tacticalreportbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacticalReportBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
